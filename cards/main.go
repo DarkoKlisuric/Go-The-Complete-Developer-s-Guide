945d8c3cf830056e55dee33ef9f4b7b2592f86d3
DarkoKlisuric <klisuric1995@gmail.com>
2020-01-06 13:52:09 +0000

    Initialization

---
+++ cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//var card string = "Ace of spades" initialization
+	card := "Ace of spades" // short initialization
+	card = "Five of diamonds"
+	var newVariable int
+	newVariable = 25
+	fmt.Println(newVariable)
+	fmt.Println(card)
+}
