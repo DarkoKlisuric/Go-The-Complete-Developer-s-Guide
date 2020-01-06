a3eb192080cad1b186896a01a0b2903b87cf8750
DarkoKlisuric <klisuric1995@gmail.com>
2020-01-06 18:28:43 +0000

    More testing

--- CardsProject/deck_test.go
+++ CardsProject/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T)  {
 	if len(d) != 52 {
 		t.Errorf("Expected deck lenght of 52 but got %d", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card King of clubs but got %v", d[len(d)-1])
+	}
 }
