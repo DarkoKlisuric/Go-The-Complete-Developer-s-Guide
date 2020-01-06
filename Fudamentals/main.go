e37da03b07f452765a16b5887f2829d358e2b04a
DarkoKlisuric <klisuric1995@gmail.com>
2020-01-06 14:12:31 +0000

    Looping

--- Fudamentals/main.go
+++ Fudamentals/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	//slices
+	cards := []string{"Ace of diamons", newCard()}
+	//add new element to slice
+	cards = append(cards, "Six of spades")
+
+	//looping with index key
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	// looping without index key
+	for _, card := range cards {
+		fmt.Println(card)
+	}
+	fmt.Println(cards)
+
 	fmt.Println(newCard())
 }
 
