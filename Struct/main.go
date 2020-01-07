5f67bf1015fe66c7a3332e8dc80bb07784ce871b
DarkoKlisuric <klisuric1995@gmail.com>
2020-01-07 19:44:45 +0000

    Embedded stuct

--- Struct/main.go
+++ Struct/main.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	yearOfBirth int
+}
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main()  {
+	/*
 	// 1# way to define struct
-	darko := person{"Darko", "Klisurić"}
+	 darko := person{"Darko", "Klisurić"}
 	// 2# way to define struct
-	klisuric := person{firstName: "Darko", lastName: "Klisurić"}
+	 klisuric := person{firstName: "Darko", lastName: "Klisurić"}
 
 	var darkoKlisuric person
 	darkoKlisuric.lastName = "Klisurić"
@@ -20,5 +26,19 @@ func main()  {
 	fmt.Printf("%+v" , darkoKlisuric)
 	fmt.Println(darkoKlisuric)
 	fmt.Println(klisuric, darko)
+	*/
+
+	// embedded struct
+	marko := person{
+		firstName: "Marko",
+		lastName:  "Markić",
+		contact:   contactInfo{
+			email: "marko@marko.com",
+			yearOfBirth: 1995,
+		},
+	}
+
+	fmt.Printf("%+v" , marko)
+
 
 }
